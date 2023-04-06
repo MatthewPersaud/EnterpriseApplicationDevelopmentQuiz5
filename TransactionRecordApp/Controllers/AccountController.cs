36e63f16b4f4568135632dacc07e0603a179cf82
MatthewPersaud <Mpersaud4116@conestogac.on.ca>
2023-04-06 00:25:02 -0400

    Add project files.

---
+++ TransactionRecordApp/Controllers/AccountController.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using TransactionRecordApp.Models;
+
+namespace TransactionRecordApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        public UserManager<User> _userManager { get; set; }
+        public SignInManager<User> _signInManager { get; set; }
+
+        /// <summary>
+        /// Class constructor
+        /// </summary>
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        /// <summary>
+        /// Returns the view for registering a new user.
+        /// </summary>
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Registers a new user
+        /// </summary>
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new User { UserName = model.Username };
+                var result = await _userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+
+            return View(model);
+        }
+
+        /// <summary>
+        /// Signs user out and redirects to home page.
+        /// </summary>
+        [HttpPost]
+        public async Task<IActionResult> LogOut()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
+        /// <summary>
+        /// Returns view for logging in.
+        /// </summary>
+        [HttpGet]
+        public IActionResult LogIn(string returnURL = "")
+        {
+            var model = new LoginViewModel { ReturnUrl = returnURL };
+            return View(model);
+        }
+
+        /// <summary>
+        /// Attempts to log the user in
+        /// </summary>
+        [HttpPost]
+        public async Task<IActionResult> LogIn(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(model.Username, model.Password,
+                            isPersistent: model.RememberMe, lockoutOnFailure: false);
+
+                if (result.Succeeded)
+                {
+                    if (!string.IsNullOrEmpty(model.ReturnUrl) && Url.IsLocalUrl(model.ReturnUrl))
+                    {
+                        return Redirect(model.ReturnUrl);
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+            }
+
+            ModelState.AddModelError("", "Invalid username/password.");
+            return View(model);
+        }
+
+        /// <summary>
+        /// Returns view for an access denied error.
+        /// </summary>
+        [HttpGet]
+        public ViewResult AccessDenied()
+        {
+            return View();
+        }
+    }
+}
